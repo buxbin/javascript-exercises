3f908429d90c84e3a68d3a2ef8cc1e691bec0e2c
buxbin <110273995+buxbin@users.noreply.github.com>
2025-08-09 21:12:00 +0200

    finished exercise 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (string, count) {
+  if (count < 0) return "ERROR";
+  let resultString = "";
+  for (let i = 0; i < count; i++) {
+    resultString += string;
+  }
+  return resultString;
 };
 
 // Do not edit below this line
