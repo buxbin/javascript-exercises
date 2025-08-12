0e38a485a4238b79346bf939f2b47bedeee4ee9c
buxbin <110273995+buxbin@users.noreply.github.com>
2025-08-13 00:21:21 +0200

    finished exercise 11_tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  let celsius = ((fahrenheit - 32) * 5) / 9;
+  celsius = celsius.toFixed(1);
+  celsius = parseFloat(celsius);
+
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  let fahrenheit = (celsius * 9) / 5 + 32;
+  fahrenheit = fahrenheit.toFixed(1);
+  fahrenheit = parseFloat(fahrenheit);
+
+  return fahrenheit;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
