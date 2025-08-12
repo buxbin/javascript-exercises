1f50fd9a90879729bb82b2ddd0567265ca567c80
buxbin <110273995+buxbin@users.noreply.github.com>
2025-08-12 23:00:21 +0200

    finished exercise 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function (array, ...remove) {
+  let result = [];
 
+  array.forEach((element) => {
+    if (!remove.includes(element)) {
+      result.push(element);
+    }
+  });
+  return result;
+};
 // Do not edit below this line
 module.exports = removeFromArray;
