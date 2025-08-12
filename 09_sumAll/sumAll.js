c65781df293c0c4b69b187fedfe3b3b43beff561
buxbin <110273995+buxbin@users.noreply.github.com>
2025-08-12 23:11:37 +0200

    finished exercise 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+  if (a < 0 || b < 0) return "ERROR";
+  if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+  let sum = 0;
+  let larger = a > b ? a : b;
+  let smaller = a > b ? b : a;
+  for (let i = larger; i >= smaller; i--) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
