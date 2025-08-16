52dbe3906fb111ecde63a857bea6bbea1f3cddb9
buxbin <110273995+buxbin@users.noreply.github.com>
2025-08-16 20:09:09 +0200

    finished exercise 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,25 @@
-const palindromes = function () {
+const palindromes = function (word) {
+  let result = "";
 
+  for (let c of word) {
+    if (/^[a-zA-Z0-9]$/.test(c)) {
+      result += c;
+    }
+  }
+
+  let pointer = result.length - 1;
+  result = result.toLowerCase();
+
+  for (let i = 0; i < result.length; i++) {
+    if (i === pointer || i > pointer) return true;
+
+    if (result[i] === result[pointer]) {
+      pointer--;
+      continue;
+    } else {
+      return false;
+    }
+  }
 };
 
 // Do not edit below this line
