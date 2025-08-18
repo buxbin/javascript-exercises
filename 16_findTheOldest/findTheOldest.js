d88f69a315ec23621ebba5cb8ca3eae8e08fc662
buxbin <110273995+buxbin@users.noreply.github.com>
2025-08-18 12:05:28 +0200

    finished exercise 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+  const date = new Date();
 
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].yearOfDeath === undefined) {
+      array[i].yearOfDeath = date.getFullYear();
+    }
+  }
+
+  array.sort((personA, personB) =>
+    personA.yearOfDeath - personA.yearOfBirth >
+    personB.yearOfDeath - personB.yearOfBirth
+      ? 1
+      : -1
+  );
+
+  return array[array.length - 1];
 };
 
 // Do not edit below this line
